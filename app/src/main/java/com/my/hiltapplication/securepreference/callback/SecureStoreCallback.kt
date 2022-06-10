c05dba54d3bc49bd1d89192250dd255dda773b49
이상현 <sanghyun.lee@deleo.co.kr>
2022-06-10 17:05:13 +0900

    add comment

--- app/src/main/java/com/my/hiltapplication/securepreference/callback/SecureStoreCallback.kt
+++ app/src/main/java/com/my/hiltapplication/securepreference/callback/SecureStoreCallback.kt
@@ -1,7 +1,10 @@
 package com.my.hiltapplication.securepreference.callback
 
 /**
- * 로컬 store preference 완료 콜백
+ * 로컬 store preference 완료 콜백.
+ * 주의!!!!!!
+ * 사용시 받듯이 호출되는 Coroutines Scope가 Default인점을 상기 시키고 UI 동작이나 ViewModel LiveData 업데이트 시에
+ * 적절한 Scope를 설정해 주어야 한다.
  */
 interface SecureStoreCallback {
     fun storeFinish()
