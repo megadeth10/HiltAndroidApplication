8f7feff6b44120b7ce134929fcd37b569c74b0dc
이상현 <sanghyun.lee@deleo.co.kr>
2022-06-17 15:27:28 +0900

    activity test 코드 수정함. 동작 확인 완료.

--- app/src/androidTest/java/com/my/hiltapplication/scene/SpendListActivityTest.kt
+++ app/src/androidTest/java/com/my/hiltapplication/scene/SpendListActivityTest.kt
@@ -1,7 +1,9 @@
 package com.my.hiltapplication.scene
 
+import android.content.Context
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
@@ -13,6 +15,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.google.common.truth.Truth.assertThat
 import com.my.hiltapplication.R
+import com.my.hiltapplication.room.SpendDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.hamcrest.Matchers.allOf
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,10 +39,16 @@ class SpendListActivityTest {
         activityScenario = launchActivity()
         activityScenario.moveToState(Lifecycle.State.RESUMED)
     }
-//
-//    @After
-//    fun tearDown() {
-//    }
+
+    @After
+    fun tearDown() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            val db = SpendDatabase.invoke(context)
+            db.clearAllTables()
+            db.close()
+        }
+    }
 
     @Test
     fun testLaunchActivity() {
@@ -48,14 +62,14 @@ class SpendListActivityTest {
 
     @Test
     fun testAddSpendItem() {
-        val amount = 12
-        val description = "hie"
+        val amount = 2
+        val description = "bbb"
         onView(withId(R.id.et_amount)).perform(typeText(amount.toString()))
         onView(withId(R.id.et_description)).perform(typeText(description))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.btn_action)).perform(click())
-        onView(withText(amount.toString())).check(matches(isDisplayed()))
-        onView(withText(description)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_amount)).check(matches(allOf(isDisplayed(), withText(amount.toString()))))
+        onView(withId(R.id.tv_description)).check(matches(allOf(isDisplayed(), withText(description))))
 //        assertThat(onView(withId(R.id.et_description)).check(matches(withText(description)))).isEqualTo(true)
     }
 }
